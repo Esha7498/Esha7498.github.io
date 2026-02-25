ac9db23cf0ddfe2b52f77050b827923a476070bd
Esha7498 <80858877+Esha7498@users.noreply.github.com>
2026-02-24 21:34:38 -0500

    Update App.tsx

--- Data Scientist Portfolio/src/app/App.tsx
+++ Data Scientist Portfolio/src/app/App.tsx
@@ -1,6 +1,7 @@
 import { Navigation } from './components/Navigation';
 import { MainContent } from './components/MainContent';
 import { Experience } from './components/Experience';
+import { Trainings } from './components/Trainings';
 import { Skills } from './components/Skills';
 import { Footer } from './components/Footer';
 
@@ -11,6 +12,7 @@ export default function App() {
       <div className="pt-16">
         <MainContent />
         <Experience />
+        <Trainings />
         <Skills />
         
         
